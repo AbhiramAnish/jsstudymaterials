c3405b54d4d9c1d3442fe03b817d50c660b0073f
AbhiramAnish <abhiramanish2016@gmail.com>
2024-05-19 11:53:25 +0530

    done

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const Base_URL ="https://v6.exchangerate-api.com/v6/9bbcc2bf9f5388c6360fa2d9/latest";
 
 const dropdown= document.querySelectorAll(".dropdown select");
-const flags1= document.querySelector(".selectFrom");
-const flags2= document.querySelector(".selectTo")  
-const image1 = document.querySelector(".flagsFrom");
-const image2 = document.querySelector(".flagsTo");
 const btn = document.querySelector("form button");
 
 for (select of dropdown){
@@ -21,16 +17,16 @@ for (select of dropdown){
         }
     }
 }
-
-
-flags1.addEventListener("click",()=>{
-    console.log(flags1.value);
-    image1.src=`https://flagsapi.com/${flags1.value}/shiny/64.png`;
+select.addEventListener("change",(evt)=>{
+    updateFlag(evt);
 })
-flags2.addEventListener("click",()=>{
-    console.log(flags2.value);
-    image2.src=`https://flagsapi.com/${flags2.value}/shiny/64.png`;
-})
+
+const updateFlag = (element) => {
+    let countryCode = countryList[element.value];
+    let img = element.parentElement.querySelector("img");
+    img.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  };
+
 
 btn.addEventListener("click",(evt)=>{
     evt.preventDefault(); // to prevent the default behaviour of the the form that is the refreshing of the page when submitted
@@ -41,5 +37,3 @@ btn.addEventListener("click",(evt)=>{
     }
 })
 
-const URL=`${Base_URL}/${flags1.value}`
-console.log(URL);
